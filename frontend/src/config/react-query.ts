8becde2ef3f9b82c6af015e52e0048f35f976372
alikhani <alikhani.dev@gmail.com>
2023-07-07 12:13:39 +0330

    remove staleTime config react query

--- frontend/src/config/react-query.ts
+++ frontend/src/config/react-query.ts
@@ -1,11 +1,8 @@
 import { QueryClient } from '@tanstack/react-query'
 
-const twentyFourHoursInMs = 1000 * 60 * 60 * 24
-
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			staleTime: twentyFourHoursInMs,
 			refetchOnWindowFocus: false,
 			refetchOnReconnect: false,
 			retry: false
