62777e9644d9f6c211f948c9c2ef01a4cd63f191
alikhani <alikhani.dev@gmail.com>
2023-07-07 12:21:57 +0330

    fix props type course box

--- frontend/src/components/CourseBox/CourseBox.tsx
+++ frontend/src/components/CourseBox/CourseBox.tsx
@@ -1,32 +1,28 @@
 import { faArrowLeft, faChalkboardTeacher, faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { CircleSpinner, StarRating } from '..'
+import { CoursesResponse } from '../../api/courses.api'
 import { toFarsiNumber } from '../../helper/number'
+import { CircleSpinner, StarRating } from '..'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import './CourseBox.css'
 
-type Props = {
-	image: string
-	users: number
-	name: string
-	rating: number
-	price: number
-	teacher: string
-	toCourse: string
-	toProfile: string
-}
-
-const CourseBox: React.FC<Props> = ({ name, teacher, rating, toCourse, toProfile, price, users, image }) => {
+const CourseBox: React.FC<CoursesResponse> = ({ cover, creator, name, price, shortName }) => {
 	const [loader, setLoader] = useState(false)
-
 	const onImageLoaded = () => setLoader(true)
 
 	return (
 		<div className='col-4'>
 			<div className='course-box'>
-				<Link to='/'>
-					<img src={image} alt='Course img' className='course-box__img' onLoad={onImageLoaded} />
+				<Link to={`/course-info/${shortName}`}>
+					<img
+						src={cover}
+						alt='Course img'
+						className='course-box__img'
+						onLoad={onImageLoaded}
+						width={300}
+						height={200}
+					/>
 					{!loader && <CircleSpinner />}
 				</Link>
 				<div className='course-box__main'>
@@ -36,22 +32,22 @@ const CourseBox: React.FC<Props> = ({ name, teacher, rating, toCourse, toProfile
 					<div className='course-box__rating-teacher'>
 						<div className='course-box__teacher'>
 							<FontAwesomeIcon icon={faChalkboardTeacher} className='course-box__teacher-icon' />
-							<Link to={toProfile} className='course-box__teacher-link'>
-								{teacher}
+							<Link to='/' className='course-box__teacher-link'>
+								{creator}
 							</Link>
 						</div>
-						<StarRating rating={rating} />
+						<StarRating rating={4} />
 					</div>
 					<div className='course-box__status'>
 						<div className='course-box__users'>
 							<FontAwesomeIcon icon={faUser} className='course-box__users-icon' />
-							<span className='course-box__users-text'>{toFarsiNumber(users)}</span>
+							<span className='course-box__users-text'>{toFarsiNumber(1)}</span>
 						</div>
-						<span className='course-box__price'>{toFarsiNumber(price)}</span>
+						<span className='course-box__price'>{toFarsiNumber(+price)}</span>
 					</div>
 				</div>
 				<div className='course-box__footer'>
-					<Link to={toCourse} className='course-box__footer-link'>
+					<Link to={`/course-info/${shortName}`} className='course-box__footer-link'>
 						مشاهده اطلاعات
 						<FontAwesomeIcon icon={faArrowLeft} className='course-box__footer-icon' />
 					</Link>
